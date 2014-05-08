b3734b5b5edb474bb2a9a171961e09ca912b2c9d
bl-test-account <plugin.developer@bytelight.com>
2014-05-08 16:35:12 -0700

    Released on: 2014-05-08 19:35:11 -0400

--- preview/page.js
+++ preview/page.js
@@ -4,7 +4,6 @@ BL.getContext.The returned object contains the lightGroupId,
 lightGroupName, and userId properties.
 */
 context = BL.getContext();
-
  /*
 Retrieve static content with Bl.getStaticContent.
 Static content is stored as an object with properties
@@ -12,13 +11,6 @@ equal to the identifiers of each piece of content. Each property
 contains an array with the content corresponding
 to each update of the app.
 */
-savedContent = BL.getStaticContent();
-$.each(savedContent, function(identifier, contentArray) {
-  /*
-  Add the most recent static content update by accessing the first
-  element in the array for each piece of content.
-  */
-  if (typeof contentArray  != 'undefined') {
-    $(img).attr('src', contentArray[0])
-  }
-});
+savedContent = BL.getStaticContent(APP.appId)//(APP.appId);
+$('.content-image').attr('src', savedContent.iphone_image_url[0])
+
